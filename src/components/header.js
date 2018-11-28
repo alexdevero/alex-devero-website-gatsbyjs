f265b2041559e83981afaf3b8387f61292381d28
Alex Devero <deveroalex@gmail.com>
2018-11-28 05:55:20 +0100

    Add footer, header, image and layout components

---
+++ src/components/header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const Header = () => (
+  <header className="header" style={{ display: 'none' }}>
+    <nav className="nav-main">
+      <ul>
+        <li>
+          <Link to="/projects/">Projects</Link>
+        </li>
+
+        <li>
+          <a href="https://blog.alexdevero.com/">Blog</a>
+        </li>
+
+        <li>
+          <Link to="/about/">About</Link>
+        </li>
+
+        <li>
+          <Link to="/contact/">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+export default Header
