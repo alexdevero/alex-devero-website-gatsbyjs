4a566c49b27ada1e6d729e414a74d4d81991bbc8
Alex Devero <deveroalex@gmail.com>
2018-11-30 10:39:07 +0100

    Update content of Projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,8 +3,162 @@ import React from 'react'
 import Layout from '../components/layout'
 
 const ProjectsPage = () => (
-  <Layout>
-    <h1>Welcome to my personal laboratory</h1>
+  <Layout page="projects">
+    <div className="hero">
+      <h1>Projects</h1>
+    </div>
+
+    <div className="container">
+      <p>
+        Welcome to my personal laboratory. These are some of my recent experiments. You can find the code for most of them is on my <a href="https://github.com/alexdevero">GitHub</a>.
+      </p>
+
+      <div className="row">
+        <div className="col-md-6 col-lg-4">
+          <a className="project__container" href="#">
+            <div className="project__header">
+              <img className="project__thumbnail" src="" alt="" />
+            </div>
+
+            <div className="project__body">
+              <span className="project__number">
+                No.<span>1</span>
+              </span>
+
+              <h2 className="project__heading h5">Project heading</h2>
+            </div>
+          </a>
+        </div>
+
+        <div className="col-md-6 col-lg-4">
+          <a className="project__container" href="#">
+            <div className="project__header">
+              <img className="project__thumbnail" src="" alt="" />
+            </div>
+
+            <div className="project__body">
+              <span className="project__number">
+                No.<span>2</span>
+              </span>
+
+              <h2 className="project__heading h5">Project heading</h2>
+            </div>
+          </a>
+        </div>
+
+        <div className="col-md-6 col-lg-4">
+          <a className="project__container" href="#">
+            <div className="project__header">
+              <img className="project__thumbnail" src="" alt="" />
+            </div>
+
+            <div className="project__body">
+              <span className="project__number">
+                No.<span>3</span>
+              </span>
+
+              <h2 className="project__heading h5">Project heading</h2>
+            </div>
+          </a>
+        </div>
+
+        <div className="col-md-6 col-lg-4">
+          <a className="project__container" href="#">
+            <div className="project__header">
+              <img className="project__thumbnail" src="" alt="" />
+            </div>
+
+            <div className="project__body">
+              <span className="project__number">
+                No.<span>4</span>
+              </span>
+
+              <h2 className="project__heading h5">Project heading</h2>
+            </div>
+          </a>
+        </div>
+
+        <div className="col-md-6 col-lg-4">
+          <a className="project__container" href="#">
+            <div className="project__header">
+              <img className="project__thumbnail" src="" alt="" />
+            </div>
+
+            <div className="project__body">
+              <span className="project__number">
+                No.<span>5</span>
+              </span>
+
+              <h2 className="project__heading h5">Project heading</h2>
+            </div>
+          </a>
+        </div>
+
+        <div className="col-md-6 col-lg-4">
+          <a className="project__container" href="#">
+            <div className="project__header">
+              <img className="project__thumbnail" src="" alt="" />
+            </div>
+
+            <div className="project__body">
+              <span className="project__number">
+                No.<span>6</span>
+              </span>
+
+              <h2 className="project__heading h5">Project heading</h2>
+            </div>
+          </a>
+        </div>
+
+        <div className="col-md-6 col-lg-4">
+          <a className="project__container" href="#">
+            <div className="project__header">
+              <img className="project__thumbnail" src="" alt="" />
+            </div>
+
+            <div className="project__body">
+              <span className="project__number">
+                No.<span>7</span>
+              </span>
+
+              <h2 className="project__heading h5">Project heading</h2>
+            </div>
+          </a>
+        </div>
+
+        <div className="col-md-6 col-lg-4">
+          <a className="project__container" href="#">
+            <div className="project__header">
+              <img className="project__thumbnail" src="" alt="" />
+            </div>
+
+            <div className="project__body">
+              <span className="project__number">
+                No.<span>8</span>
+              </span>
+
+              <h2 className="project__heading h5">Project heading</h2>
+            </div>
+          </a>
+        </div>
+
+        <div className="col-md-6 col-lg-4">
+          <a className="project__container" href="#">
+            <div className="project__header">
+              <img className="project__thumbnail" src="" alt="" />
+            </div>
+
+            <div className="project__body">
+              <span className="project__number">
+                No.<span>9</span>
+              </span>
+
+              <h2 className="project__heading h5">Project heading</h2>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
