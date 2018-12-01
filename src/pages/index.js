d9ce7d183fd7d6861625fc7a4d116f9776254410
Alex Devero <deveroalex@gmail.com>
2018-12-01 07:00:55 +0100

    Update index page grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,13 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout page="home">
     <div className="container">
-      <h1>Alex Devero</h1>
+      <div className="row justify-content-center">
+        <div className="col-lg-8">
+          <h1>Alex Devero</h1>
 
-      <p>Entrepreneur, designer and developer from Czech Republic.</p>
+          <p>Entrepreneur, designer and developer from Czech Republic.</p>
+        </div>
+      </div>
     </div>
   </Layout>
 )
