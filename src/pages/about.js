6d38c7884ec7f2da635e4fb51a53d611c76cd181
Alex Devero <deveroalex@gmail.com>
2018-12-01 06:59:55 +0100

    Update about page grid

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,17 +9,21 @@ const AboutPage = () => (
     </div>
 
     <div className="container">
-      <p>I am founder/CEO of DEVERO Corporation. An entrepreneur, self-taught designer &amp; developer from Czech Republic. I am obsessed with all things business, technology and design and often step inside related fields.</p>
+      <div className="row justify-content-center">
+        <div className="col-lg-8">
+          <p>I am founder/CEO of DEVERO Corporation. An entrepreneur, self-taught designer &amp; developer from Czech Republic. I am obsessed with all things business, technology and design and often step inside related fields.</p>
 
-      <p>
-        My journey in designing and developing websites, building interactive experiences and creating businesses began when I was 11. Beside work, I write extensively on{' '}
-        <a className="link--red link--underline" href="https://blog.alexdevero.com/">
-          my blog
-        </a>
-        .
-      </p>
+          <p>
+            My journey in designing and developing websites, building interactive experiences and creating businesses began when I was 11. Beside work, I write extensively on{' '}
+            <a className="link--red link--underline" href="https://blog.alexdevero.com/">
+              my blog
+            </a>
+            .
+          </p>
 
-      <p>My goal is to use technology to make positive impact on the World and humankind, and change both in the process.</p>
+          <p>My goal is to use technology to make positive impact on the World and humankind, and change both in the process.</p>
+        </div>
+      </div>
     </div>
   </Layout>
 )
