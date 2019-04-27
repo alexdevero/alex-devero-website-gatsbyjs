5585dfb8bd2fded2d88c6742dcce6ffb37b0bce3
Alex Devero <deveroalex@gmail.com>
2019-04-27 10:38:28 +0200

    Update about page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import SEO from '../components/seo'
 
 const AboutPage = () => (
   <Layout>
-    <SEO title="About" keywords={['about', 'design', 'web design', 'entrepreneur', 'founder', 'ceo']} />
+    <SEO title="About" keywords={['about', 'design', 'web design', 'startup', 'startups', 'entrepreneur', 'founder', 'ceo']} />
 
     <div className="hero">
       <h1>About</h1>
@@ -14,17 +14,9 @@ const AboutPage = () => (
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-lg-8">
-          <p>I am founder/CEO of DEVERO Corporation. An entrepreneur, self-taught designer &amp; developer from Czech Republic. I am obsessed with all things business, technology and design and often step inside related fields.</p>
+          <p>I am founder/CEO of DEVERO Corporation. Entrepreneur, innovator, hacker, tinkerer, developer, programmer, designer, philosopher, blogger, poly(math | glot) and heretic from Czech Republic. I am obsessed with all things startups, business, technology, design and beyond. Beside working on <a href="https://devero.io" target="_blank">DEVERO Corporation</a>, I write extensively on <a className="link--red link--underline" href="https://blog.alexdevero.com/">my blog</a>.</p>
 
-          <p>
-            My journey in designing and developing websites, building interactive experiences and creating businesses began when I was 11. Beside work, I write extensively on{' '}
-            <a className="link--red link--underline" href="https://blog.alexdevero.com/">
-              my blog
-            </a>
-            .
-          </p>
-
-          <p>My goal is to use technology to make positive impact on the World and humankind, and change both in the process.</p>
+          <p>My goal is to create the biggest company in the history and use technology to change the World and humankind.</p>
         </div>
       </div>
     </div>
