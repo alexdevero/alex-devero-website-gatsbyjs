985dfa8bba1ff7e77bd93cb7d99d3a73fe888845
Alex Devero <deveroalex@gmail.com>
2019-04-28 10:28:35 +0200

    Update About page info

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,9 +14,9 @@ const AboutPage = () => (
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-lg-8">
-          <p>I am founder/CEO of DEVERO Corporation. Entrepreneur, innovator, hacker, tinkerer, developer, programmer, designer, philosopher, blogger, poly(math | glot) and heretic from Czech Republic. I am obsessed with all things startups, business, technology, design and beyond. Beside working on <a href="https://devero.io" target="_blank" rel="noopener noreferrer">DEVERO Corporation</a>, I write extensively on <a className="link--red link--underline" href="https://blog.alexdevero.com/" target="_blank" rel="noopener noreferrer">my blog</a>.</p>
+          <p>I am founder/CEO of DEVERO Corporation. Entrepreneur, innovator, hacker, tinkerer, developer, programmer, designer, philosopher, blogger, poly(math | glot) and heretic from Czech Republic. I am obsessed with all things startups, business, technology, design and beyond. My goal is to create the biggest company in the history and use technology to change the World and humankind.</p>
 
-          <p>My goal is to create the biggest company in the history and use technology to change the World and humankind.</p>
+          <p>Beside working on <a href="https://devero.io" target="_blank" rel="noopener noreferrer">DEVERO Corporation</a>, I write extensively on <a className="link--red link--underline" href="https://blog.alexdevero.com/" target="_blank" rel="noopener noreferrer">my blog</a>. About social media ... I am very active on <a href="https://twitter.com/alexdevero" target="_blank" rel="noopener noreferrer">Twitter</a> where are share my tips, ideas and everything I learned so far about startups, design and technology and interesting things and news. I also like to share interesting things and news on <a href="https://www.instagram.com/alexdevero/" target="_blank" rel="noopener noreferrer">Instagram</a>.</p>
         </div>
       </div>
     </div>
