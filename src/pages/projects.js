8eba4a47f561646752270e975801cdb2fe8bd4f2
Alex Devero <deveroalex@gmail.com>
2019-05-21 16:15:01 +0200

    Add work examples (thumbnails) from Dribbble

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import ImageZoom from 'react-medium-image-zoom'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -17,155 +19,261 @@ const ProjectsPage = () => (
           <p>
             Welcome to my personal laboratory. These are some of my experiments. Code for most of them is on my <a href="https://github.com/alexdevero">GitHub</a>.
           </p>
-
-          <h2 className="projects__wip">Work in progress</h2>
         </div>
       </div>
 
       <div className="projects__dashboard row">
         <div className="col-md-6 col-lg-4">
-          <button className="project__container" href="#">
+          <div className="project__container" to="#">
             <div className="project__header">
-              <img className="project__thumbnail" src="" alt="" />
+              <ImageZoom
+                image={{
+                  src: require('./../images/thumbnails/thumbnail-tesla-web-design-concept.jpg'),
+                  alt: 'Tesla web design concept',
+                  className: 'project__thumbnail'
+                }}
+                zoomImage={{
+                  src: require('./../images/thumbnails/thumbnail-tesla-web-design-concept.jpg'),
+                  alt: 'Tesla web design concept'
+                }}
+              />
             </div>
 
             <div className="project__body">
-              <span className="project__number">
-                No.<span>1</span>
-              </span>
+              <Link to="/projects/" className="project__link">
+                <span className="project__number">
+                  No.<span>1</span>
+                </span>
 
-              <h2 className="project__heading h5">Project heading</h2>
+                <h5 className="project__heading h5">Tesla web design concept</h5>
+              </Link>
             </div>
-          </button>
+          </div>
         </div>
 
         <div className="col-md-6 col-lg-4">
-          <button className="project__container" href="#">
+          <div className="project__container" to="#">
             <div className="project__header">
-              <img className="project__thumbnail" src="" alt="" />
+              <ImageZoom
+                image={{
+                  src: require('./../images/thumbnails/thumbnail-harley-davidson.jpg'),
+                  alt: 'Harley Davidson web design concept',
+                  className: 'project__thumbnail'
+                }}
+                zoomImage={{
+                  src: require('./../images/thumbnails/thumbnail-harley-davidson.jpg'),
+                  alt: 'Harley Davidson web design concept'
+                }}
+              />
             </div>
 
             <div className="project__body">
-              <span className="project__number">
-                No.<span>2</span>
-              </span>
+              <Link to="/projects/" className="project__link">
+                <span className="project__number">
+                  No.<span>2</span>
+                </span>
 
-              <h2 className="project__heading h5">Project heading</h2>
+                <h5 className="project__heading h5">Harley Davidson web design concept</h5>
+              </Link>
             </div>
-          </button>
+          </div>
         </div>
 
         <div className="col-md-6 col-lg-4">
-          <button className="project__container" href="#">
+          <div className="project__container" to="#">
             <div className="project__header">
-              <img className="project__thumbnail" src="" alt="" />
+              <ImageZoom
+                image={{
+                  src: require('./../images/thumbnails/thumbnail-ceska-whisky.jpg'),
+                  alt: 'Ceska Whisky web design concept',
+                  className: 'project__thumbnail'
+                }}
+                zoomImage={{
+                  src: require('./../images/thumbnails/thumbnail-ceska-whisky.jpg'),
+                  alt: 'Ceska Whisky web design concept'
+                }}
+              />
             </div>
 
             <div className="project__body">
-              <span className="project__number">
-                No.<span>3</span>
-              </span>
+              <Link to="/projects/" className="project__link">
+                <span className="project__number">
+                  No.<span>3</span>
+                </span>
 
-              <h2 className="project__heading h5">Project heading</h2>
+                <h5 className="project__heading h5">Ceska Whisky web design concept</h5>
+              </Link>
             </div>
-          </button>
+          </div>
         </div>
 
-        <div className="col-md-6 col-lg-4">
-          <button className="project__container" href="#">
+        {/* <div className="col-md-6 col-lg-4">
+          <div className="project__container" to="#">
             <div className="project__header">
-              <img className="project__thumbnail" src="" alt="" />
+              <ImageZoom
+                image={{
+                  src: require('./../images/thumbnails/'),
+                  alt: '',
+                  className: 'project__thumbnail'
+                }}
+                zoomImage={{
+                  src: require('./../images/thumbnails/'),
+                  alt: ''
+                }}
+              />
             </div>
 
             <div className="project__body">
-              <span className="project__number">
-                No.<span>4</span>
-              </span>
+              <Link to="" className="project__link">
+                <span className="project__number">
+                  No.<span>4</span>
+                </span>
 
-              <h2 className="project__heading h5">Project heading</h2>
+                <h5 className="project__heading h5">Project heading</h5>
+              </Link>
             </div>
-          </button>
-        </div>
+          </div>
+        </div> */}
 
-        <div className="col-md-6 col-lg-4">
-          <button className="project__container" href="#">
+        {/* <div className="col-md-6 col-lg-4">
+          <div className="project__container" to="#">
             <div className="project__header">
-              <img className="project__thumbnail" src="" alt="" />
+              <ImageZoom
+                image={{
+                  src: require('./../images/thumbnails/'),
+                  alt: '',
+                  className: 'project__thumbnail'
+                }}
+                zoomImage={{
+                  src: require('./../images/thumbnails/'),
+                  alt: ''
+                }}
+              />
             </div>
 
             <div className="project__body">
-              <span className="project__number">
-                No.<span>5</span>
-              </span>
+              <Link to="" className="project__link">
+                <span className="project__number">
+                  No.<span>5</span>
+                </span>
 
-              <h2 className="project__heading h5">Project heading</h2>
+                <h5 className="project__heading h5">Project heading</h5>
+              </Link>
             </div>
-          </button>
-        </div>
+          </div>
+        </div> */}
 
-        <div className="col-md-6 col-lg-4">
-          <button className="project__container" href="#">
+        {/* <div className="col-md-6 col-lg-4">
+          <div className="project__container" to="#">
             <div className="project__header">
-              <img className="project__thumbnail" src="" alt="" />
+              <ImageZoom
+                image={{
+                  src: require('./../images/thumbnails/'),
+                  alt: '',
+                  className: 'project__thumbnail'
+                }}
+                zoomImage={{
+                  src: require('./../images/thumbnails/'),
+                  alt: ''
+                }}
+              />
             </div>
 
             <div className="project__body">
-              <span className="project__number">
-                No.<span>6</span>
-              </span>
+              <Link to="" className="project__link">
+                <span className="project__number">
+                  No.<span>6</span>
+                </span>
 
-              <h2 className="project__heading h5">Project heading</h2>
+                <h5 className="project__heading h5">Project heading</h5>
+              </Link>
             </div>
-          </button>
-        </div>
+          </div>
+        </div> */}
 
-        <div className="col-md-6 col-lg-4">
-          <button className="project__container" href="#">
+        {/* <div className="col-md-6 col-lg-4">
+          <div className="project__container" to="#">
             <div className="project__header">
-              <img className="project__thumbnail" src="" alt="" />
+              <ImageZoom
+                image={{
+                  src: require('./../images/thumbnails/'),
+                  alt: '',
+                  className: 'project__thumbnail'
+                }}
+                zoomImage={{
+                  src: require('./../images/thumbnails/'),
+                  alt: ''
+                }}
+              />
             </div>
 
             <div className="project__body">
-              <span className="project__number">
-                No.<span>7</span>
-              </span>
+              <Link to="" className="project__link">
+                <span className="project__number">
+                  No.<span>7</span>
+                </span>
 
-              <h2 className="project__heading h5">Project heading</h2>
+                <h5 className="project__heading h5">Project heading</h5>
+              </Link>
             </div>
-          </button>
-        </div>
+          </div>
+        </div> */}
 
-        <div className="col-md-6 col-lg-4">
-          <button className="project__container" href="#">
+        {/* <div className="col-md-6 col-lg-4">
+          <div className="project__container" to="#">
             <div className="project__header">
-              <img className="project__thumbnail" src="" alt="" />
+              <ImageZoom
+                image={{
+                  src: require('./../images/thumbnails/'),
+                  alt: '',
+                  className: 'project__thumbnail'
+                }}
+                zoomImage={{
+                  src: require('./../images/thumbnails/'),
+                  alt: ''
+                }}
+              />
             </div>
 
             <div className="project__body">
-              <span className="project__number">
-                No.<span>8</span>
-              </span>
+              <Link to="" className="project__link">
+                <span className="project__number">
+                  No.<span>8</span>
+                </span>
 
-              <h2 className="project__heading h5">Project heading</h2>
+                <h5 className="project__heading h5">Project heading</h5>
+              </Link>
             </div>
-          </button>
-        </div>
+          </div>
+        </div> */}
 
-        <div className="col-md-6 col-lg-4">
-          <button className="project__container" href="#">
+        {/* <div className="col-md-6 col-lg-4">
+          <div className="project__container" to="#">
             <div className="project__header">
-              <img className="project__thumbnail" src="" alt="" />
+              <ImageZoom
+                image={{
+                  src: require('./../images/thumbnails/'),
+                  alt: '',
+                  className: 'project__thumbnail'
+                }}
+                zoomImage={{
+                  src: require('./../images/thumbnails/'),
+                  alt: ''
+                }}
+              />
             </div>
 
             <div className="project__body">
-              <span className="project__number">
-                No.<span>9</span>
-              </span>
+              <Link to="" className="project__link">
+                <span className="project__number">
+                  No.<span>9</span>
+                </span>
 
-              <h2 className="project__heading h5">Project heading</h2>
+                <h5 className="project__heading h5">Project heading</h5>
+              </Link>
             </div>
-          </button>
-        </div>
+          </div>
+        </div> */}
       </div>
     </div>
   </Layout>
