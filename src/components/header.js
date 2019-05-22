9a0cdb112d46d18dd212a4cbe0b60f0e5532c1a9
Alex Devero <deveroalex@gmail.com>
2019-05-22 11:51:08 +0200

    Add mobile navigation

--- src/components/header.js
+++ src/components/header.js
@@ -1,40 +1,65 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-const Header = () => (
-  <header className="header">
+const Header = () => {
+  const [isMobileMenuVisible, toggleMobileMenu] = React.useState(false)
+
+  const generateNavClass = () => {
+    if (window.innerWidth <= 767.99 && isMobileMenuVisible) {
+      return 'nav-main--visible'
+    } else if (window.innerWidth <= 767.99 && !isMobileMenuVisible) {
+      return 'nav-main--hidden'
+    } else {
+      return null
+    }
+  }
+
+  return <header className="header">
     <nav className="nav-main">
-      <ul>
+      <ul className={generateNavClass()}>
         <li>
-          <Link to="/">Home</Link>
+          <button className="nav__toggler nav__toggler--close" onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>
+            <span></span>
+            <span></span>
+          </button>
         </li>
 
         <li>
-          <Link to="/projects/">Projects</Link>
+          <Link to="/" onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>Home</Link>
         </li>
 
         <li>
-          <Link to="/about/">About</Link>
+          <Link to="/projects/" onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>Projects</Link>
         </li>
 
         <li>
-          <a href="https://blog.alexdevero.com/" rel="noopener noreferrer" target="_blank">Blog</a>
+          <Link to="/about/" onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>About</Link>
         </li>
 
         <li>
-          <a href="https://devero.io/" rel="noopener noreferrer" target="_blank">Devero</a>
+          <a href="https://blog.alexdevero.com/" rel="noopener noreferrer" target="_blank" onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>Blog</a>
         </li>
 
         <li>
-          <a href="https://studio.devero.io/" rel="noopener noreferrer" target="_blank">Studio</a>
+          <a href="https://devero.io/" rel="noopener noreferrer" target="_blank" onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>Devero</a>
         </li>
 
         <li>
-          <Link to="/contact/">Contact</Link>
+          <a href="https://studio.devero.io/" rel="noopener noreferrer" target="_blank" onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>Studio</a>
+        </li>
+
+        <li>
+          <Link to="/contact/" onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>Contact</Link>
         </li>
       </ul>
+
+      <button className="nav__toggler nav__toggler--toggle" onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
     </nav>
   </header>
-)
+}
 
 export default Header
